b94418b9ffa7243bca1c3c80fde3d186181e64c4
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2024-05-21 09:21:35 -0300

    add layout pages - components app code-videos

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // appDir: true, depois do next 14 não é mais necessario
+  },
+};
+
+module.exports = nextConfig;
