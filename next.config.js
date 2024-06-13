bbdbcecc1754eb5bcfb7182af38840047325e942
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2024-06-13 15:52:41 -0300

    add banner video

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     // appDir: true, depois do next 14 não é mais necessario
   },
+  images: {
+    domains: ['m.media-amazon.com'],
+  },
 };
 
 module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { Banner } from './components/Banner';
 import Header from './components/Header';
 import { MovieRow } from './components/MovieRow';
+import { getFeaturedMovie } from './service/MovieService';
 
-export default function Home() {
+export default async function Home() {
+  const featureMovie = await getFeaturedMovie('106');
   return (
     <div className='relative bg-gradient-to-b pb-8'>
       <Header />
       <main className='relative mb-48 h-screen pl-4 lg:pl-16 '>
-        <Banner />
+        <Banner movie={featureMovie} />
         <MovieRow sectionTitle='Popular' />
         <MovieRow sectionTitle='Trending' />
         <MovieRow sectionTitle='Top Rated' />
