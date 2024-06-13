e429aff92fffb05128da785da564e40d6a8872c5
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2024-06-13 16:30:20 -0300

    conect movies genres api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
 import { Banner } from './components/Banner';
 import Header from './components/Header';
 import { MovieRow } from './components/MovieRow';
-import { getFeaturedMovie } from './service/MovieService';
+import { getFeaturedMovie, getMoviesByGenre } from './service/MovieService';
 
 export default async function Home() {
   const featureMovie = await getFeaturedMovie('106');
+  const genres = ['Drama', 'Comedy', 'Action', 'Animation'];
+
+  const movies = await Promise.all(
+    genres.map(async (genre) => {
+      const movies = await getMoviesByGenre(genre, { _limit: 10 });
+      return { sectionTitle: genre, movies };
+    })
+  );
+
   return (
     <div className='relative bg-gradient-to-b pb-8'>
       <Header />
       <main className='relative mb-48 h-screen pl-4 lg:pl-16 '>
         <Banner movie={featureMovie} />
-        <MovieRow sectionTitle='Popular' />
-        <MovieRow sectionTitle='Trending' />
-        <MovieRow sectionTitle='Top Rated' />
+        {movies.map((movie) => (
+          <MovieRow
+            key={movie.sectionTitle}
+            sectionTitle={movie.sectionTitle}
+            movies={movie.movies}
+          />
+        ))}
       </main>
     </div>
   );
