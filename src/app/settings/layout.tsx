a5c0fccf1966fb10701a502a7d461159ed1081b3
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2024-06-25 10:45:32 -0300

    fix: build fail pages

--- src/app/settings/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div lang='en'>
-      <h1 className='text-3xl '>layout Whapper</h1>
-      <div className=' border border-double border-sky-500 p-4'>{children}</div>
-    </div>
-  );
-}
